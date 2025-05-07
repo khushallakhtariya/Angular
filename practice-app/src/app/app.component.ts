1333ec54b5da42a0c946d4887dc2fe6e8aa85836
unknown <you@example.com>
2025-05-07 22:29:41 +0530

    routes set

--- practice-app/src/app/app.component.ts
+++ practice-app/src/app/app.component.ts
@@ -1,39 +1,15 @@
 import { Component } from "@angular/core";
-import { RouterLink, RouterOutlet } from "@angular/router";
+import { RouterOutlet } from "@angular/router";
 import { NavComponent } from "./nav/nav.component";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
-import { ContactComponent } from "./contact/contact.component";
-import { HelpMeComponent } from "./help-me/help-me.component";
 import { FooterComponent } from "./footer/footer.component";
-import { NoPageComponent } from "./no-page/no-page.component";
-import { InfoComponent } from "./get-info/get-info.component";
-
-
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    NavComponent,
-    HomeComponent,
-    AboutComponent,
-    ContactComponent, 
-    HelpMeComponent,
-    FooterComponent,
-    NoPageComponent,
-    InfoComponent
-
-  
-  
-  ],
+  imports: [RouterOutlet, NavComponent, FooterComponent],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   title = "practice-app";
 }
-
-
--- practice-app/src/app/home/home.component.ts
+++ practice-app/src/app/home/home.component.ts
@@ -1,77 +1,84 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { RouterModule, RouterLink } from '@angular/router';
+import { Component, ElementRef, ViewChild, AfterViewInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { RouterModule, RouterLink } from "@angular/router";
+
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
   imports: [CommonModule, RouterModule, RouterLink], // Required if you're using standalone components
 })
-export class HomeComponent {
-  @ViewChild('heroVideo') heroVideoRef!: ElementRef<HTMLVideoElement>;
+export class HomeComponent implements AfterViewInit {
+  @ViewChild("heroVideo") heroVideoRef!: ElementRef<HTMLVideoElement>;
   isVideoPlaying = true;
 
   cars = [
     {
-      code: 'car-1',
-      name: 'Porsche 911',
+      code: "car-1",
+      name: "Porsche 911",
       year: 2021,
-      price: 'Rs. 1.99 - 4.26 Crore',
+      price: "Rs. 1.99 - 4.26 Crore",
       description:
-        'The price of Porsche 911, a 2-seater Coupe & Convertible, ranges from Rs. 1.99 - 4.26 Crore. It is available in 8 variants, with engine options ranging from 2981 to 3996 cc and a choice of 1 transmission: Automatic. 911 comes with 7 airbags and is available in 15 colours.',
+        "The price of Porsche 911, a 2-seater Coupe & Convertible, ranges from Rs. 1.99 - 4.26 Crore. It is available in 8 variants, with engine options ranging from 2981 to 3996 cc and a choice of 1 transmission: Automatic. 911 comes with 7 airbags and is available in 15 colours.",
       rating: 4.8,
       image:
-        'https://imgd.aeplcdn.com/664x374/n/cw/ec/39232/911-exterior-right-front-three-quarter-154382.jpeg?isig=0&wm=1&q=80',
+        "https://imgd.aeplcdn.com/664x374/n/cw/ec/39232/911-exterior-right-front-three-quarter-154382.jpeg?isig=0&wm=1&q=80",
     },
     {
-      code: 'car-2',
-      name: 'Porsche Macan',
+      code: "car-2",
+      name: "Porsche Macan",
       year: 2021,
-      price: 'Rs. 96.05 Lakh',
+      price: "Rs. 96.05 Lakh",
       description:
-        'The price of Porsche Macan, a 5 seater Full-Size SUV, starts from of Rs. 96.05 Lakh. It is available in 1 variant, with an engine of 1984 cc and a choice of 1 transmission: Automatic. Macan has an NCAP rating of 5 stars and comes with 8 airbags. Porsche Macan is available in 9 colours.',
+        "The price of Porsche Macan, a 5 seater Full-Size SUV, starts from of Rs. 96.05 Lakh. It is available in 1 variant, with an engine of 1984 cc and a choice of 1 transmission: Automatic. Macan has an NCAP rating of 5 stars and comes with 8 airbags. Porsche Macan is available in 9 colours.",
       rating: 4.8,
       image:
-        'https://imgd.aeplcdn.com/664x374/n/cw/ec/99421/macan-exterior-right-front-three-quarter-9.jpeg?isig=0&q=80',
+        "https://imgd.aeplcdn.com/664x374/n/cw/ec/99421/macan-exterior-right-front-three-quarter-9.jpeg?isig=0&q=80",
     },
     {
-      code: 'car-3',
-      name: 'Porsche Cayenne',
+      code: "car-3",
+      name: "Porsche Cayenne",
       year: 2021,
-      price: 'Rs. 1.42 - 2.00 Crore',
+      price: "Rs. 1.42 - 2.00 Crore",
       description:
-        'The price of Porsche Cayenne, a 5 seater Full-Size SUV, ranges from Rs. 1.42 - 2.00 Crore. It is available in 2 variants, with engine options ranging from 2995 to 3996 cc and a choice of 1 transmission: Automatic. Cayenne has an NCAP rating of 5 stars and comes with 9 airbags.',
+        "The price of Porsche Cayenne, a 5 seater Full-Size SUV, ranges from Rs. 1.42 - 2.00 Crore. It is available in 2 variants, with engine options ranging from 2995 to 3996 cc and a choice of 1 transmission: Automatic. Cayenne has an NCAP rating of 5 stars and comes with 9 airbags.",
       rating: 4.8,
       image:
-        'https://imgd.aeplcdn.com/664x374/n/cw/ec/32951/cayenne-exterior-right-front-three-quarter-2.jpeg?isig=0&wm=1&q=80',
+        "https://imgd.aeplcdn.com/664x374/n/cw/ec/32951/cayenne-exterior-right-front-three-quarter-2.jpeg?isig=0&wm=1&q=80",
     },
     {
-      code: 'car-4',
-      name: 'Porsche Taycan',
+      code: "car-4",
+      name: "Porsche Taycan",
       year: 2021,
-      price: 'Rs. 1.67 - 2.54 Crore',
+      price: "Rs. 1.67 - 2.54 Crore",
       description:
         "The Taycan is Porsche's first electric car. It packs a huge battery with enormous power output and can hit 100kmph in just 2.6 seconds. It is both practical and exhilarating to drive.",
       rating: 4.8,
       image:
-        'https://imgd.aeplcdn.com/664x374/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80',
+        "https://imgd.aeplcdn.com/664x374/n/cw/ec/45063/taycan-exterior-right-front-three-quarter-5.jpeg?isig=0&q=80",
     },
     {
-      code: 'car-5',
-      name: 'Porsche Panamera',
+      code: "car-5",
+      name: "Porsche Panamera",
       year: 2021,
-      price: 'Rs. 1.70 - 2.34 Crore',
+      price: "Rs. 1.70 - 2.34 Crore",
       description:
-        'The price of Porsche Panamera, a 4 seater Sedan, ranges from Rs. 1.70 - 2.34 Crore. It is available in 2 variants, with engine options ranging from 2894 to 3996 cc and a choice of 1 transmission: Automatic. Panamera comes with 10 airbags.',
+        "The price of Porsche Panamera, a 4 seater Sedan, ranges from Rs. 1.70 - 2.34 Crore. It is available in 2 variants, with engine options ranging from 2894 to 3996 cc and a choice of 1 transmission: Automatic. Panamera comes with 10 airbags.",
       rating: 4.8,
       image:
-        'https://imgd.aeplcdn.com/664x374/n/cw/ec/165641/panamera-exterior-right-front-three-quarter.jpeg?isig=0&q=80',
+        "https://imgd.aeplcdn.com/664x374/n/cw/ec/165641/panamera-exterior-right-front-three-quarter.jpeg?isig=0&q=80",
     },
   ];
 
-  constructor(private router: Router) {}
+  carName: string = "";
+
+  // Combine the constructors into one
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   ngAfterViewInit(): void {
     const video = this.heroVideoRef.nativeElement;
@@ -81,7 +88,7 @@ export class HomeComponent {
         this.isVideoPlaying = true;
       })
       .catch((error) => {
-        console.error('Video failed to autoplay:', error);
+        console.error("Video failed to autoplay:", error);
       });
   }
 
@@ -97,4 +104,9 @@ export class HomeComponent {
     }
   }
 
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.carName = params.get("name") || "";
+    });
+  }
 }
