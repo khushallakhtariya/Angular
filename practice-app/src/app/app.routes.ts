617e8468d1482967ce03d559d26f1ef956a8c5d5
khushallakhtariya <khushallakhatariya@gmail.com>
2025-05-08 15:14:07 +0530

    refactor routing paths and update button functionality in view car details component

--- practice-app/src/app/app.routes.ts
+++ practice-app/src/app/app.routes.ts
@@ -1,58 +1,59 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
+import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 // import { ViewCarDetailsComponent } from "./view-car-details/view-car-details.component";
 
 export const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: HomeComponent,
   },
   {
-    path: "view/:code/details",
+    path: 'view/:code/details',
     loadComponent: () =>
-      import("./view-car-details/view-car-details.component").then(
-        (m) => m.ViewCarDetailsComponent,
+      import('./view-car-details/view-car-details.component').then(
+        (m) => m.ViewCarDetailsComponent
       ),
+    children: [
+      {
+        path: 'view/car/details/:code/:section',
+        loadComponent: () =>
+          import('./view-car-details/view-car-details.component').then(
+            (m) => m.ViewCarDetailsComponent
+          ),
+      },
+    ],
   },
-  {
-    path: "view/car/details/:code/:section",
-    loadComponent: () =>
-      import("./view-car-details/view-car-details.component").then(
-        (m) => m.ViewCarDetailsComponent,
-      ),
-  },
-
   {
     path: 'view/:code/details/info',
     loadComponent: () =>
       import('./get-info/get-info.component').then((m) => m.InfoComponent),
   },
- 
+
   {
-    path: "about",
+    path: 'about',
     loadComponent: () =>
-      import("./about/about.component").then((m) => m.AboutComponent),
+      import('./about/about.component').then((m) => m.AboutComponent),
   },
   {
-    path: "contact",
+    path: 'contact',
     loadComponent: () =>
-      import("./contact/contact.component").then((m) => m.ContactComponent),
+      import('./contact/contact.component').then((m) => m.ContactComponent),
   },
 
   {
-    path: "help-me",
+    path: 'help-me',
     loadComponent: () =>
-      import("./help-me/help-me.component").then((m) => m.HelpMeComponent),
+      import('./help-me/help-me.component').then((m) => m.HelpMeComponent),
   },
   {
-    path: "login",
+    path: 'login',
     loadComponent: () =>
-      import("./login/login.component").then((m) => m.LoginComponent),
+      import('./login/login.component').then((m) => m.LoginComponent),
   },
 
   {
-    path: "**",
+    path: '**',
     loadComponent: () =>
-      import("./no-page/no-page.component").then((m) => m.NoPageComponent),
+      import('./no-page/no-page.component').then((m) => m.NoPageComponent),
   },
 ];
